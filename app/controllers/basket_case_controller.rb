058356d7895c52a1c2a04a34c5e045771bbd60e5
Cedric <calindasjohn@gmail.com>
2024-12-13 18:57:38 +0800

    Added sort by amount and added Ascending and Descending Toggle on the buttons

--- app/controllers/basket_case_controller.rb
+++ app/controllers/basket_case_controller.rb
@@ -5,29 +5,36 @@ class BasketCaseController < ApplicationController
   def index; end
 
   def order_by_fruit
-    @list_items = @list_items.sort
-    @list_items = @list_items.reverse
+    if params[:order] == "desc"
+      @list_items = @list_items.sort.reverse # Sort descending
+    else
+      @list_items = @list_items.sort # Sort ascending
+    end
     render 'basket_case/index'
   end
 
   def order_by_amount
-    # code here
-
+    if params[:order] == "desc"
+      @list_items = @list_items.sort_by(&:last).reverse # Sort by amount descending
+    else
+      @list_items = @list_items.sort_by(&:last) # Sort by amount ascending
+    end
     render 'basket_case/index'
   end
 
+ 
   private
-    def set_fruits
-      @fruits = {
-        oranges: 5,
-        apples: 9,
-        grapes: 3,
-        watermelons: 2,
-        guavas: 4
-      }
-    end
 
-    def set_items
-      @list_items = @fruits
-    end
+  def set_fruits
+    @fruits = {
+      oranges: 5,
+      apples: 9,
+      grapes: 3,
+      watermelons: 2
+    }
+  end
+
+  def set_items
+    @list_items = @fruits
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def toggle_order(attribute)
+        if params[:order] == "desc"
+          "asc"  # Toggle to ascending if it's descending
+        else
+          "desc"  # Toggle to descending if it's ascending or not set
+        end
+      end 
 end
